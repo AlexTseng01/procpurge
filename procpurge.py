293234cfb25d0fc5d702173b9f0efc12f9b6d9c4
AlexTseng01 <atseng1@umbc.edu>
2026-06-09 15:27:48 -0400

    added list_processes and list_actions

--- procpurge.py
+++ procpurge.py
@@ -1,13 +1,21 @@
+# FUTURE ADDONS:
+# 3. Command: autostart (basically autostarts upon computer reboot)
+# 4. Guardrails for the purge command since it's too powerful apparently
+# 5. Protected tasks for, you know, avoid being terminated, lol.
+# 6. Command: lock/unlock process, which basically adds or removes a process from the protected tasks list 
+# 7. append_proc should be able to take multiple arguments
+# 8. remove_proc should be able to take multiple arguments
+
 import psutil
 import sys
 import argparse
 
-# Appends a process to the process list
+# Appends a process to the purge list
 def append_proc(process):
     with open("processes.txt", "a") as file:
         file.write(process + "\n")
 
-# Removes a process from the process list
+# Removes a process from the purge list
 def remove_proc(process):
     with open("processes.txt", "r") as file:
         lines = file.readlines()
@@ -17,7 +25,23 @@ def remove_proc(process):
             if process != line.strip():
                 file.write(line)
 
-# Purge all tasks from the process list
+# Lists all processes in the purge list
+def list_processes():
+    with open("processes.txt", "r") as file:
+        for line in file.readlines():
+            print(line.strip())
+
+# Lists all processes in the purge list
+def list_actions():
+    print(
+        "add\n" + 
+        "remove\n" + 
+        "list_processes\n" + 
+        "list_actions\n" + 
+        "purge"
+    )
+
+# Purge all tasks from the purge list
 def purge():
     with open("processes.txt", "r") as file:
         target_processes = [line.strip().lower() for line in file if line.strip()]
@@ -39,12 +63,14 @@ def main(command, name):
     ACTION_MAP = {
         "add": append_proc,
         "remove": remove_proc,
+        "list_processes": list_processes,
+        "list_actions": list_actions,
         "purge": purge
     }
 
     action_name = ACTION_MAP[command]
 
-    if command == "purge":
+    if command == "purge" or command == "list_processes" or command == "list_actions":
         action_name()
     else:
         if not name:
@@ -55,7 +81,7 @@ def main(command, name):
 # Parser
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="ProcPurge")
-    parser.add_argument("command", choices=["add", "remove", "purge"], help="Actions to execute")
+    parser.add_argument("command", choices=["add", "remove", "list_processes", "list_actions", "purge"], help="Actions to execute")
     parser.add_argument("name", nargs="?", default=None, help="Process target name")
     args = parser.parse_args()
     main(args.command, args.name)
